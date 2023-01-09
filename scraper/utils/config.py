85e9c40902dd0e62f19c8282e389c76e85e63bcb
andreamarin <andrea.marin2411@gmail.com>
2023-01-08 22:07:30 -0600

    go back to headless mode

--- scraper/utils/config.py
+++ scraper/utils/config.py
@@ -9,7 +9,7 @@ MAIN_URL = "https://pleno.senado.gob.mx/infosen/infosen64/index.php?c=Legislatur
 BASE_URL = "https://www.senado.gob.mx"
 BASE_URL_V2 = "https://pleno.senado.gob.mx/infosen/infosen64"
 
-HEADLESS = False
+HEADLESS = True
 DOWNLOAD_PATH = os.path.join(os.getcwd(), "downloads")
 
 TABLE_XPATH = '//*[@id="viewDataBase"]/div'

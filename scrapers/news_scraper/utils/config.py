a1cfe40acbca70e1ccebe9f3728fff494af305ea
andreamarin <andrea.marin2411@gmail.com>
2024-04-24 00:02:33 -0600

    set locale in config file

--- scrapers/news_scraper/utils/config.py
+++ scrapers/news_scraper/utils/config.py
@@ -1,3 +1,9 @@
+import locale
+
+# set locale language to ES
+locale.setlocale(locale.LC_TIME, "es_ES")
+
+# global variables
 OUT_PATH = "./data/news/"
 IDS_PATH = "./data/ids/{newspaper}/"
 CHECKPOINT_PATH = "./data/checkpoints/{newspaper}/"

15177c5d3222f0964ac6cc7f1126bcdc83c3bd21
Andrea Marín <andrea.marin2411@gmail.com>
2026-02-02 20:40:43 -0600

    Add: news analysis (#8)
    * update requirements file
    
    * update requirements file
    
    * remove config symlink
    
    * enable extra processing functions
    
    * add news notebook
    
    * fix variable name
    
    * add numpy to requirements¨
    
    * add gensim to requirements
    
    * fix numpy version
    
    * add preprocessing logic
    
    * update utils
    
    * add batch processing
    
    * add requirements for colab
    
    * change requirements order
    
    * update nb
    
    * update notebook
    
    * add lda models
    
    * create dirs
    
    * update notebook
    
    * parametrize model passes
    
    * fix range and coherence scores list reference
    
    * latest version
    
    * Se creó con Colab
    
    * Se creó con Colab
    
    * add function for printing topics
    
    * fix requirements
    
    * fix model id
    
    * fix model references
    
    * add load for corpus and dict
    
    * print topics
    
    * Se creó con Colab
    
    * add code to fix section names
    
    * remove punctuation marks
    
    * add frequency analysis
    
    * use updated timestamp
    
    * update functions
    
    * latest changes
    
    * Update nlp_processor.py
    
    fix process_doc function (keep the previous step output)
    
    * Update nlp_processor.py
    
    handle unicode punctuation
    
    * latest changes
    
    * Se creó con Colab
    
    * Se creó con Colab
    
    * add new stopwords

--- nlp_classification/utils/lda_model_builder.py
+++ nlp_classification/utils/lda_model_builder.py
@@ -1,5 +1,4 @@
 import os
-import spacy
 import pickle
 import pathlib
 import numpy as np
@@ -16,24 +15,30 @@ class LDAModelBuilder:
     def __init__(
         self,
         texts_df: pd.DataFrame,
-        spacy_model_name: str,
         folder_name: str,
         num_topics_config: dict,
+        base_path: str = None,
+        model_passes: int = 300
     ):
-        self._nlp = spacy.load(spacy_model_name)
         self._texts_df = texts_df
 
         self._topics_range = range(
             num_topics_config.get("min_topics", 3),
-            num_topics_config.get("max_topics", 15),
+            num_topics_config.get("max_topics", 15) + 1,
             num_topics_config.get("step_size", 3),
         )
 
         current_path = pathlib.Path(__file__).parent.resolve()
-        base_path = current_path.parent.resolve()
+        base_path = current_path.parent.resolve() if base_path is None else base_path
 
         self._images_path = f"{base_path}/{folder_name}/coherence_scores"
         self._models_path = f"{base_path}/{folder_name}/models"
+
+        self._model_passes = model_passes
+
+        self.dictionary = None
+        self.corpus = None
+        self.models = dict()
 
     def _generate_models(
         self, model_id: str, model_data: pd.Series, force_replacement: bool = False
@@ -41,6 +46,8 @@ class LDAModelBuilder:
         words = list(model_data.apply(str.split))
 
         base_dir = f"{self._models_path}/{model_id}"
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
 
         # dictionary of words
         dictionary_path = f"{base_dir}/dictionary.pkl"
@@ -80,8 +87,14 @@ class LDAModelBuilder:
             else:
                 # create lda model
                 model = LdaModel(
-                    corpus, num_topics=num_topics, id2word=dictionary, passes=300
+                    corpus, num_topics=num_topics, id2word=dictionary, passes=self._model_passes
                 )
+
+                # save model to dictionary
+                if model_id not in self.models:
+                    self.models[model_id] = dict()
+
+                self.models[model_id][num_topics] = model
 
                 # save model to drive
                 model.save(model_path)
@@ -91,12 +104,12 @@ class LDAModelBuilder:
                 num_topics=num_topics, num_words=10
             )
 
-        # calculate coherence score
-        cm = CoherenceModel(
-            model=model, texts=words, dictionary=dictionary, coherence="c_v"
-        )
-        coherence_lda = cm.get_coherence()
-        self.coherence_scores.append(coherence_lda)
+            # calculate coherence score
+            cm = CoherenceModel(
+                model=model, texts=words, dictionary=dictionary, coherence="c_v"
+            )
+            coherence_lda = cm.get_coherence()
+            self._coherence_scores.append(coherence_lda)
 
     def _create_coherence_plot(self, model_id: str):
         plt.figure(figsize=(10, 6))
@@ -152,34 +165,77 @@ class LDAModelBuilder:
         plt.savefig(f"{self._images_path}/{img_name}.png", format="png", dpi=300)
         plt.show()
 
-    def load_model_results(self, text_type, num_topics, filter: str = "all"):
-        model_id = f"{text_type}_{filter}"
-        base_dir = f"{self._models_path}/{model_id}"
+    def _load_dictionary(self, model_id: str):
+        """
+        Load the dictionary and corpus for the given model
 
+        Parameters
+        ----------
+        model_dir : str
+            path where the dictionary and corpus are saved
+        """
         # dictionary of words
-        dictionary_path = f"{base_dir}/dictionary.pkl"
+        dictionary_path = f"{self._models_path}/{model_id}/dictionary.pkl"
+
+        if not os.path.exists(dictionary_path):
+            raise ValueError(f"Dictionary for model doesn't exist at: {dictionary_path}")
+
         self.dictionary = pickle.load(open(dictionary_path, "rb"))
 
         # corpus with bag of words
-        corpus_path = f"{base_dir}/corpus.pkl"
+        corpus_path = f"{self._models_path}/{model_id}/corpus.pkl"
+
+        if not os.path.exists(corpus_path):
+            raise ValueError(f"Corpus for model doesn't exist at: {corpus_path}")
+        
         self.corpus = pickle.load(open(corpus_path, "rb"))
-
+    
+    def _load_model(self, model_id: str, num_topics: int):
         # load model
-        model_path = f"{base_dir}/{num_topics}_topics/lda"
-        self.model = LdaModel.load(model_path)
+        model_path = f"{self._models_path}/{model_id}/{num_topics}_topics/lda"
 
-    def generate_model_data(self, num_topics):
+        if self.models.get(model_id, dict()).get(num_topics, None):
+            # model already loaded
+            return
+
+        if not os.path.exists(model_path):
+            raise ValueError(f"Model doesn't exist at: {model_path}")
+        
+        if model_id not in self.models:
+            self.models[model_id] = dict()
+
+        self.models[model_id][num_topics] = LdaModel.load(model_path)
+
+    def generate_model_vis(self, text_type: str, num_topics: int, filter: str = "all"):
+
+        model_id = f"{text_type}_{filter}"
+
+        # load the needed data
+        self._load_model(model_id, num_topics)
+        self._load_dictionary(model_id)
+
+        model = self.models[model_id][num_topics]
+
         # create df with topics
         topics_df = pd.DataFrame()
-        for num_topic, topic_words in self.model.print_topics(
+        for num_topic, topic_words in model.print_topics(
             num_topics=num_topics, num_words=10
         ):
             topics_df[f"topic_{num_topic}"] = topic_words.split(" + ")
 
         # generalte topic visualization
-        vis = pyLDAvis.gensim.prepare(self.model, self.corpus, self.dictionary)
+        vis = pyLDAvis.gensim.prepare(model, self.corpus, self.dictionary)
 
         return topics_df, vis
+    
+    def set_model(self, text_type: str, num_topics: int, filter: str = "all"):
+        model_id = f"{text_type}_{filter}"
+        self._load_model(model_id, num_topics)
+
+        self.model = self.models.get(model_id, dict()).get(num_topics)
+
+        if self.model is None:
+            raise ValueError(f"Model not found for specs: {model_id}, {num_topics}")
 
     def get_publication_topics(self, text):
         words = text.split()
@@ -190,3 +246,22 @@ class LDAModelBuilder:
             publication_topics.append({"topic": topic, "score": float(score)})
 
         return publication_topics
+
+    def print_topics(self, text_type: str, filter: str = "all", num_topics_list: list = []):
+        """
+        Print the topics for the provided model specs 
+        """
+
+        topics_list = [num_topics_list] if num_topics_list else self._topics_range
+        model_id = f"{text_type}_{filter}"
+
+        for num_topics in topics_list:
+            print("====="*10)
+            print(f"Topics for: {model_id}, {num_topics}")
+
+            # load model output
+            self._load_model(model_id, num_topics)
+
+            # get the topics
+            for topic in self.models[model_id][num_topics].print_topics(num_topics=num_topics, num_words=10):
+                print(topic)
--- nlp_classification/utils/nlp_processor.py
+++ nlp_classification/utils/nlp_processor.py
@@ -1,16 +1,10 @@
 import re
-import os
+import regex
 import spacy
-import pickle
-import pathlib
+import string
 import numpy as np
 import pandas as pd
-import pyLDAvis.gensim
-import matplotlib.pyplot as plt
-import gensim.corpora as corpora
 from functools import partial
-from gensim.models.ldamodel import LdaModel
-from gensim.models.coherencemodel import CoherenceModel
 
 
 class NlpProcessor:
@@ -19,24 +13,13 @@ class NlpProcessor:
         self,
         texts_df: pd.DataFrame,
         spacy_model_name: str,
-        folder_name: str,
-        num_topics_config: dict,
         process_text_config: dict,
+        extra_processing_steps: list = []
     ):
         self._nlp = spacy.load(spacy_model_name)
         self._texts_df = texts_df
 
-        self._topics_range = range(
-            num_topics_config.get("min_topics", 3),
-            num_topics_config.get("max_topics", 15),
-            num_topics_config.get("step_size", 3),
-        )
-
-        current_path = pathlib.Path(__file__).parent.resolve()
-        base_path = current_path.parent.resolve()
-
-        self._images_path = f"{base_path}/{folder_name}/coherence_scores"
-        self._models_path = f"{base_path}/{folder_name}/models"
+        self._extra_processing_steps = extra_processing_steps
 
         self._remove_words = process_text_config.get("remove_words", [])
         self._stop_words = process_text_config.get("stop_words", [])
@@ -57,36 +40,39 @@ class NlpProcessor:
         """
         Clean text (create lemmas, remove special characters, remove stop words)
         """
+        processed_text = text
+
+        # execute functions
+        for function in self._extra_processing_steps:
+            processed_text = function(processed_text)
+
         # remove specific words (before lemmatizing)
         for word in self._remove_words:
-            text = text.replace(word, "")
+            processed_text = processed_text.replace(word, "")
+
+        # remove punctuation marks
+        processed_text = regex.sub(r"\p{P}+", "", processed_text)
 
         # remove multiple spaces
-        text = re.sub(r" +", " ", text)
-        text = text.strip()
+        processed_text = re.sub(r" +", " ", processed_text)
+        processed_text = processed_text.strip()
 
-        # lemmatize text
-        nlp_text = self._nlp(text)
-        lemmas = [word.lemma_ for word in nlp_text]
-
-        normalized_text = " ".join(lemmas)
-
-        # remove special characters
-        normalized_text = normalized_text.lower()
-        normalized_text = re.sub(r"[^a-záéíóúñ\d\s_]", "", normalized_text, re.A)
+        # lemmatize text and turn to lower case
+        nlp_text = self._nlp(processed_text)
+        lemmas = [word.lemma_.lower() for word in nlp_text]
 
         if self._stop_words:
             # remove stopwords
-            tokens = normalized_text.split(" ")
             filtered_tokens = [
-                token for token in tokens if token not in self._stop_words
+                token for token in lemmas if token not in self._stop_words
             ]
 
             processed_text = " ".join(filtered_tokens)
         else:
-            processed_text = normalized_text
+            # join all the tokens
+            processed_text = " ".join(lemmas)
 
-        return processed_text
+        return processed_text.strip()
 
     def _filter_by_pos(self, text: str, pos_tags: list):
         """
@@ -98,159 +84,3 @@ class NlpProcessor:
         filtered_tokens = [str(word) for word in nlp_text if word.pos_ in pos_tags]
 
         return " ".join(filtered_tokens)
-
-    def _generate_models(
-        self, model_id: str, model_data: pd.Series, force_replacement: bool = False
-    ):
-        words = list(model_data.apply(str.split))
-
-        base_dir = f"{self._models_path}/{model_id}"
-
-        # dictionary of words
-        dictionary_path = f"{base_dir}/dictionary.pkl"
-        if not force_replacement and os.path.exists(dictionary_path):
-            dictionary = pickle.load(open(dictionary_path, "rb"))
-        else:
-            dictionary = corpora.Dictionary(words)
-
-            # save dictionary
-            pickle.dump(dictionary, open(dictionary_path, "wb"))
-
-        # corpus with bag of words
-        corpus_path = f"{base_dir}/corpus.pkl"
-        if not force_replacement and os.path.exists(corpus_path):
-            corpus = pickle.load(open(corpus_path, "rb"))
-        else:
-            corpus = [dictionary.doc2bow(record) for record in words]
-
-            # save corpus
-            pickle.dump(corpus, open(corpus_path, "wb"))
-
-        self._model_topics = {}
-        self._coherence_scores = []
-
-        for num_topics in self._topics_range:
-            print(num_topics, end=", ")
-
-            # create folder where the model will be saved
-            model_dir = f"{base_dir}/{num_topics}_topics"
-            if not os.path.exists(model_dir):
-                os.makedirs(model_dir)
-
-            model_path = f"{model_dir}/lda"
-            if os.path.exists(model_path):
-                # load existing lda model
-                model = LdaModel.load(model_path)
-            else:
-                # create lda model
-                model = LdaModel(
-                    corpus, num_topics=num_topics, id2word=dictionary, passes=300
-                )
-
-                # save model to drive
-                model.save(model_path)
-
-            # save topics
-            self._model_topics[num_topics] = model.print_topics(
-                num_topics=num_topics, num_words=10
-            )
-
-        # calculate coherence score
-        cm = CoherenceModel(
-            model=model, texts=words, dictionary=dictionary, coherence="c_v"
-        )
-        coherence_lda = cm.get_coherence()
-        self.coherence_scores.append(coherence_lda)
-
-    def _create_coherence_plot(self, model_id: str):
-        plt.figure(figsize=(10, 6))
-        plt.plot(self._topics_range, self._coherence_scores, marker="o")
-        plt.xlabel("Number of Topics")
-        plt.ylabel("Coherence Score")
-        plt.title("Number of Topics vs Coherence Score")
-        plt.xticks(self._topics_range)
-        plt.grid(True)
-        plt.savefig(f"{self._images_path}/{model_id}.png", format="png", dpi=300)
-        plt.show()
-
-    def build_lda_models(self, text_type: str, filter_type: str = "all"):
-        model_id = f"{text_type}_{filter_type}"
-
-        if filter_type != "all":
-            model_data = self._texts_df.loc[self._texts_df.type == filter_type]
-        else:
-            model_data = self._texts_df
-
-        # build models for each num topic
-        self._generate_models(model_id, model_data[text_type])
-
-        # get the num of topics with the max coherence score
-        optimal_num_topics = self._topics_range[np.argmax(self._coherence_scores)]
-        print("\nOptimal number of topics:", optimal_num_topics)
-        print(
-            "Coherence Score for optimal number of topics:", max(self._coherence_scores)
-        )
-
-        # print best topics
-        print("")
-        for topic in self._model_topics[optimal_num_topics]:
-            print(topic)
-        print("")
-
-        self._create_coherence_plot(model_id)
-
-    def plot_coherence_scores_comparison(
-        self, title: str, coherence_scores_dict: dict, img_name: str
-    ):
-        plt.figure(figsize=(10, 6))
-
-        for text_type, coherence_scores in coherence_scores_dict.items():
-            plt.plot(self._topics_range, coherence_scores, marker="o", label=text_type)
-
-        plt.xlabel("Number of Topics")
-        plt.ylabel("Coherence Score")
-        plt.title(title)
-        plt.xticks(self._topics_range)
-        plt.legend()
-        plt.grid(True)
-        plt.savefig(f"{self._images_path}/{img_name}.png", format="png", dpi=300)
-        plt.show()
-
-    def load_model_results(self, text_type, num_topics, filter: str = "all"):
-        model_id = f"{text_type}_{filter}"
-        base_dir = f"{self._models_path}/{model_id}"
-
-        # dictionary of words
-        dictionary_path = f"{base_dir}/dictionary.pkl"
-        self.dictionary = pickle.load(open(dictionary_path, "rb"))
-
-        # corpus with bag of words
-        corpus_path = f"{base_dir}/corpus.pkl"
-        self.corpus = pickle.load(open(corpus_path, "rb"))
-
-        # load model
-        model_path = f"{base_dir}/{num_topics}_topics/lda"
-        self.model = LdaModel.load(model_path)
-
-    def generate_model_data(self, num_topics):
-        # create df with topics
-        topics_df = pd.DataFrame()
-        for num_topic, topic_words in self.model.print_topics(
-            num_topics=num_topics, num_words=10
-        ):
-            topics_df[f"topic_{num_topic}"] = topic_words.split(" + ")
-
-        # generalte topic visualization
-        vis = pyLDAvis.gensim.prepare(self.model, self.corpus, self.dictionary)
-
-        return topics_df, vis
-
-    def get_publication_topics(self, text):
-        words = text.split()
-        text_corpus = self.dictionary.doc2bow(words)
-
-        publication_topics = []
-        for topic, score in self.model.get_document_topics(text_corpus):
-            publication_topics.append({"topic": topic, "score": float(score)})
-
-        return publication_topics

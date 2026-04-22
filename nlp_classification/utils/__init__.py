f0cc73f8711a61be4fd11f8ba8680f07b2f8b134
andreamarin <andrea.marin2411@gmail.com>
2026-04-22 00:08:51 -0600

    fix imports

--- nlp_classification/utils/__init__.py
+++ nlp_classification/utils/__init__.py
@@ -1,7 +1,7 @@
-from bertopic_model_builder import BerTopicModelBuilder, EmbeddingConfig, UMAPConfig, HDBSCANConfig
-from bertopic_evaluator import BerTopicEvaluator
-from nlp_processor import NlpProcessor
-from lda_model_builder import LDAModelBuilder
+from .bertopic_model_builder import BerTopicModelBuilder, EmbeddingConfig, UMAPConfig, HDBSCANConfig
+from .bertopic_evaluator import BerTopicEvaluator
+from .nlp_processor import NlpProcessor
+from .lda_model_builder import LDAModelBuilder
 
 __all__ = [
     "BerTopicModelBuilder",

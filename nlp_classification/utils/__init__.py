ec2d76ad73825a19bba261c9f14680172ba5c0cc
andreamarin <andrea.marin2411@gmail.com>
2026-04-25 19:58:22 -0600

    fix init file

--- nlp_classification/utils/__init__.py
+++ nlp_classification/utils/__init__.py
@@ -1,7 +1,9 @@
-from .bertopic_model_builder import BerTopicModelBuilder, EmbeddingConfig, UMAPConfig, HDBSCANConfig
+from .bertopic_model_builder import BerTopicModelBuilder
 from .bertopic_evaluator import BerTopicEvaluator
 from .nlp_processor import NlpProcessor
 from .lda_model_builder import LDAModelBuilder
+from .bertopic_config import EmbeddingConfig, UMAPConfig, HDBSCANConfig
+from .bertopic_results_comparator import generate_metrics_comparison_graphs
 
 __all__ = [
     "BerTopicModelBuilder",
@@ -10,5 +12,6 @@ __all__ = [
     "HDBSCANConfig",
     "BerTopicEvaluator",
     "NlpProcessor",
-    "LDAModelBuilder"
+    "LDAModelBuilder",
+    "generate_metrics_comparison_graphs"
 ]

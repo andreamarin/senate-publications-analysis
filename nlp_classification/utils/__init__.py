6d42b2b224b995964fdee27dc5472408aa47e507
andreamarin <andrea.marin2411@gmail.com>
2026-04-25 19:59:03 -0600

    add missing import

--- nlp_classification/utils/__init__.py
+++ nlp_classification/utils/__init__.py
@@ -2,7 +2,7 @@ from .bertopic_model_builder import BerTopicModelBuilder
 from .bertopic_evaluator import BerTopicEvaluator
 from .nlp_processor import NlpProcessor
 from .lda_model_builder import LDAModelBuilder
-from .bertopic_config import EmbeddingConfig, UMAPConfig, HDBSCANConfig
+from .bertopic_config import EmbeddingConfig, UMAPConfig, HDBSCANConfig, DocumentRepresentation
 from .bertopic_results_comparator import generate_metrics_comparison_graphs
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     "EmbeddingConfig",
     "UMAPConfig",
     "HDBSCANConfig",
+    "DocumentRepresentation",
     "BerTopicEvaluator",
     "NlpProcessor",
     "LDAModelBuilder",
